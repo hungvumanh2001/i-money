c58cb08a571416901349559f7c90e1e94ca74ce8
hungvm.dev <hungvumanh2001@gmail.com>
2023-06-04 12:44:02 +0700

    done config

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    meta: {
+      layout: "default",
+    },
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    meta: {
+      layout: "auth",
+    },
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
