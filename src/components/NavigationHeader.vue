706c18e66d602053a3dff55cea979dddc5a052df
DESKTOP-9O360Q0\SEATECH <hungvm@seatechit.com.vn>
2023-06-05 16:32:13 +0700

    Component in Default Layout

---
+++ src/components/NavigationHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header id="header" class="py-8">
+    <div class="container mx-auto px-8">
+      <div class="flex justify-between items-center">
+        <div class="flex items-center">
+          <div class="w-10 h-10 overflow-hidden rounded-full">
+            <img
+              class="w-full h-auto object-cover"
+              src="https://i.pinimg.com/736x/c6/e5/65/c6e56503cfdd87da299f72dc416023d4.jpg"
+              size="sm"
+              alt="User's Profile Avatar"
+            />
+          </div>
+          <h1 class="text-xl font-bold ml-2">Hey, Sky Albert</h1>
+        </div>
+        <div class="flex">
+          <ul>
+            <li>
+              <i class="t2ico t2ico-notification text-2xl"></i>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
